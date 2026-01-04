39e5f60735eb38951834e36020e30d0e33ddecfb
streaam-developer <163761596+streaam-developer@users.noreply.github.com>
2026-01-05 05:04:50 +0530

    Update app.py

--- web/app.py
+++ web/app.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from flask import Flask, render_template, request, redirect, url_for
 from scripts.automation import Automation
 from scripts.database import Database
